7aa2e25637922189a2b936be8e864241a6772a96
Nurhan Abd Elghafar <Nurhan.abdelghafar@CARI-TECH.com>
2023-08-15 15:46:28 +0300

    todolist

--- src/Form.jsx
+++ src/Form.jsx
@@ -16,7 +16,7 @@ const Form = ({ addItem }) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <h4>grocery bud</h4>
+      <h4>To do list</h4>
       <div className='form-control'>
         <input
           type='text '
